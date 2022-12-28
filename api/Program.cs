51008b0041ea8dcf278d95f24fad74f7f4c442e4
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2022-12-28 17:02:57 +0800

    [HRIS-1] - [Dev] Setup Docker (#2)

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID={dbUser};Password={dbPassword};Encrypt=True;TrustServerCertificate=True;";
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  },
 }
