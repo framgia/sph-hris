99d8d2f51953e451716594d1c73a42ec64f32b38
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2023-01-23 10:42:00 +0800

    HRIS-53 [BE] Implement Edit and Delete Work Interruption Request functionality (#32)

--- api/Services/InterruptionService.cs
+++ api/Services/InterruptionService.cs
@@ -17,6 +17,18 @@ namespace api.Services
         {
             return new WorkInterruptionDTO(interruption);
         }
+        private static WorkInterruption ToWorkInterruption(UpdateInterruptionRequest interruption)
+        {
+            return new WorkInterruption
+            {
+                Id = interruption.Id,
+                WorkInterruptionTypeId = interruption.WorkInterruptionTypeId,
+                TimeOut = interruption.TimeOut != null ? TimeSpan.Parse(interruption.TimeOut) : null,
+                TimeIn = interruption.TimeIn != null ? TimeSpan.Parse(interruption.TimeIn) : null,
+                Remarks = interruption.Remarks,
+                OtherReason = interruption.OtherReason
+            };
+        }
         public async Task<List<WorkInterruptionType>> Index()
         {
             using (HrisContext context = _contextFactory.CreateDbContext())
@@ -33,6 +45,24 @@ namespace api.Services
                             .Where(i => i.TimeEntryId == interruption.TimeEntryId)
                             .Select(x => ToWorkInterruptionDTO(x))
                             .ToListAsync();
+            }
+        }
+        public async Task<bool> Update(UpdateInterruptionRequest interruption)
+        {
+            using (HrisContext context = _contextFactory.CreateDbContext())
+            {
+                WorkInterruption work = ToWorkInterruption(interruption);
+                context.Update(work);
+                context.Entry(work).Property(x => x.TimeEntryId).IsModified = false;
+                return await context.SaveChangesAsync() > 0;
+            }
+        }
+        public async Task<bool> Delete(int Id)
+        {
+            using (HrisContext context = _contextFactory.CreateDbContext())
+            {
+                context.Entry(new WorkInterruption { Id = Id }).State = EntityState.Deleted;
+                return await context.SaveChangesAsync() > 0;
             }
         }
         public async Task<WorkInterruptionDTO> Create(CreateInterruptionRequest interruption)
