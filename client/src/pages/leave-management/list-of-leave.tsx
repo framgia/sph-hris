4e9040ecb9149c87ec2aa8071666a3cb3569ec25
acatzk <joshuaimalay@gmail.com>
2023-02-08 18:59:07 +0800

    HRIS-107 [BugTask] Remove Filter markup and implement search function

--- client/src/pages/leave-management/list-of-leave.tsx
+++ client/src/pages/leave-management/list-of-leave.tsx
@@ -1,12 +1,14 @@
 import moment from 'moment'
 import { NextPage } from 'next'
+import classNames from 'classnames'
 import React, { useState, useEffect } from 'react'
 
 import { getLeave } from '~/hooks/useLeaveQuery'
+import { IListOfLeave } from '~/utils/interfaces'
 import ListOfLeaveTable from '~/components/molecules/ListOfLeaveTable'
 import { columns } from '~/components/molecules/ListOfLeaveTable/columns'
+import GlobalSearchFilter from '~/components/molecules/GlobalSearchFilter'
 import LeaveManagementLayout from '~/components/templates/LeaveManagementLayout'
-import { IListOfLeave } from '~/utils/interfaces'
 
 const ListOfLeave: NextPage = (): JSX.Element => {
   const [globalFilter, setGlobalFilter] = useState<string>('')
@@ -39,6 +41,18 @@ const ListOfLeave: NextPage = (): JSX.Element => {
   return (
     <LeaveManagementLayout metaTitle="List of leave">
       <section className="default-scrollbar relative overflow-auto text-xs text-slate-800">
+        <header
+          className={classNames(
+            'sticky left-0 top-0 z-20 flex items-center justify-between',
+            'border-b border-slate-200 bg-slate-100 px-4 py-2 text-xs'
+          )}
+        >
+          <GlobalSearchFilter
+            value={globalFilter ?? ''}
+            onChange={(value) => setGlobalFilter(String(value))}
+            placeholder="Search"
+          />
+        </header>
         <ListOfLeaveTable
           {...{
             query: {
