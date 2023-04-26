3ad3c697759cd78025398579ebca2b6afbe74923
John Roy Cabezas <johnroy.cabezas@sun-asterisk.com>
2023-04-26 17:25:37 +0800

    Fix: Try catcher

--- api/Services/EmployeeScheduleService.cs
+++ api/Services/EmployeeScheduleService.cs
@@ -214,27 +214,29 @@ namespace api.Services
         }
         public async Task<string> Delete(DeleteEmployeeScheduleRequest request, HrisContext context)
         {
-            ValidateDeleteRequest(request);
-            var employeeSchedule = await context.EmployeeSchedules.Where(x => x.Id == request.EmployeeScheduleId).FirstAsync();
-            var ifHasEmployee = await context.Users.Where(x => x.EmployeeScheduleId == request.EmployeeScheduleId).FirstOrDefaultAsync();
-            if (ifHasEmployee == null)
-            {
-                context.EmployeeSchedules.Remove(employeeSchedule);
-            }
-            else
-            {
-                throw new GraphQLException(ErrorBuilder.New()
-                    .SetMessage(ErrorMessageEnum.FAILED_SCHEDULE_DELETE_USER)
-                    .Build());
-            }
+            var error = ErrorMessageEnum.FAILED_SCHEDULE_DELETE;
             try
             {
+                ValidateDeleteRequest(request);
+                var employeeSchedule = await context.EmployeeSchedules.Where(x => x.Id == request.EmployeeScheduleId).FirstAsync();
+                var ifHasEmployee = await context.Users.Where(x => x.EmployeeScheduleId == request.EmployeeScheduleId).FirstOrDefaultAsync();
+                if (ifHasEmployee == null)
+                {
+                    context.EmployeeSchedules.Remove(employeeSchedule);
+                }
+                else
+                {
+                    error = ErrorMessageEnum.FAILED_SCHEDULE_DELETE_USER;
+                    throw new GraphQLException(ErrorBuilder.New()
+                        .SetMessage(ErrorMessageEnum.FAILED_SCHEDULE_DELETE_USER)
+                        .Build());
+                }
                 await context.SaveChangesAsync();
             }
             catch
             {
                 throw new GraphQLException(ErrorBuilder.New()
-                                    .SetMessage(ErrorMessageEnum.FAILED_SCHEDULE_DELETE)
+                                    .SetMessage(error)
                                     .Build());
             }
             return SuccessMessageEnum.SCHEDULE_DELETED;
