4c2f86a8c45b7bdf48e5a53b0f443c77ce9ed212
arden-dave <111718037+arden-dave@users.noreply.github.com>
2023-05-05 16:25:19 +0800

    HRIS-234 [FE] Add Secure Site Environment Variable (#201)

--- client/src/utils/ssr.ts
+++ client/src/utils/ssr.ts
@@ -5,7 +5,10 @@ export async function getServerSideProps({
 }: {
   req: NextApiRequest
 }): Promise<{ props: { cookies: string | null } }> {
-  const cookies = req.cookies['next-auth.session-token'] ?? null
+  const cookies =
+    req.cookies[
+      `${process.env.NEXT_PUBLIC_SECURE_SITE === 'true' ? '__Secure-' : ''}next-auth.session-token`
+    ] ?? null
 
   return { props: { cookies } }
 }
