22dc7335c6a7d0b4f1c07b8da7b9f1c616e3c474
acatzk <joshuaimalay@gmail.com>
2023-06-14 11:17:02 +0800

    HRIS-307 [Markup] Modify the color of the application's logo to match the company's logo.

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -5,7 +5,7 @@ const Document = (): JSX.Element => {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/sun-color-icon.svg" />
+        <link rel="icon" href="/company-logo.jpg" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
