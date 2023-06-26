1feb71c27b37bb769a8e71bfd89dfd99fc0d98f5
ncj <109492180+NiloJr-sun@users.noreply.github.com>
2023-06-26 15:09:18 +0800

    HRIS-310 [FE] Add Maintenance Page to across all pages during maintenance (#267)

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,11 +1,28 @@
-import { NextResponse, NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
+import { NextResponse, NextRequest } from 'next/server'
+import { isMaintenanceStatus } from '~/utils/constants/isMaintenanceStatus'
 
 export default async function middleware(req: NextRequest): Promise<NextResponse | undefined> {
+  const { pathname } = new URL(req.url)
+  const isUnderMaintenancePage = pathname === '/under-maintenance'
   const session = await getToken({ req, secret: process.env.NEXT_PUBLIC_JWT_SECRET })
+  const isMaintenanceVariableSet = process.env.NEXT_PUBLIC_UNDER_MAINTENANCE !== undefined
+  const isUnderMaintenance = process.env.NEXT_PUBLIC_UNDER_MAINTENANCE === isMaintenanceStatus.TRUE
 
-  if (session == null) {
-    return NextResponse.rewrite(new URL('/sign-in', req.url))
+  const rewriteURL = (path: string): NextResponse => {
+    return NextResponse.rewrite(new URL(path, req.url))
+  }
+
+  if (isUnderMaintenancePage) {
+    return rewriteURL('/404')
+  }
+
+  if (isUnderMaintenance && isMaintenanceVariableSet) {
+    return rewriteURL('/under-maintenance')
+  }
+
+  if (session === null) {
+    return rewriteURL('/sign-in')
   }
 }
 
---
+++ client/src/pages/under-maintenance.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import MaintenancePage from '~/components/pages/MaintenancePage'
+
+const Index: NextPage = (): JSX.Element => {
+  return <MaintenancePage />
+}
+
+export default Index
